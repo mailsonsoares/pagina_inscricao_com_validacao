9270e46c137a2963d3a0e06a0f7f1dd28b2fc33d
mailson <bsi.mailson@gmail.com>
2021-02-25 20:38:14 -0400

    Alterando validação para o validity dos elementos

--- js/app.js
+++ js/app.js
@@ -3,19 +3,22 @@ import valida from './validacao.js'
 const newUser = document.querySelector('[data-tipo-button]'); 
 
 newUser.addEventListener('click', (eventoDoClick) => {    
-    // eventoDoClick.preventDefault();
+    eventoDoClick.preventDefault();
 
-    console.log('----- 1 app.js -----');
+    // console.log('----- 1 app.js -----');
 
     const todosInputs = document.querySelectorAll('input'); //pega todos os inputs  
-    console.log(todosInputs);
+    //console.log(todosInputs);
 
+    // let i = 1;
     todosInputs.forEach( input => { //intera pelos inputs enviando-os, um a um, a function 'valida';
     
         // input.addEventListener('click', (eventoDoClick) => {
-            console.log('----- forEach -----');
-            
+            // console.log(`----- forEach ${i}----`);    
+            //console.log(input);        
             valida(input);
+            
+            // i++;
         // })
     })
 
--- js/validacao.js
+++ js/validacao.js
@@ -2,10 +2,18 @@
 function valida(input){ // função que recebe os inputs e checa no objeto o tipo de input
                                //para chamar a function de validação adequada ao input
 
-    console.log('---- 2 function valida in validacao.js ----')
-    const tipoDeInput = input.dataset.tipo //usa o 'dataset' para receber o data-attribut do input
+    // console.log('---- 2 function valida in validacao.js ----')
+    // const tipoDeInput = input.dataset.tipo //usa o 'dataset' para receber o data-attribut do input
 
-   
+    //console.log(input.validity);
+
+    console.log(input.validity.valid);
+
+    if(input.validity.valid){//propriedade valid do input. Se estiver 'false' está vazio.
+        input.parentElement.classList.remove('input-container--invalido');
+    }else{
+        input.parentElement.classList.add('input-container--invalido');
+    }
 
 }
 
