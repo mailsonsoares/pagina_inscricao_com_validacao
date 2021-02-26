11c44b81cbd15ab303012647ec91bcda851dbce0
mailson <bsi.mailson@gmail.com>
2021-02-26 18:45:18 -0400

    ultimos ajustes da pagina com validação de campos

--- js/app.js
+++ js/app.js
@@ -3,26 +3,15 @@ import valida from './validacao.js'
 const newUser = document.querySelector('[data-tipo-button]'); 
 
 newUser.addEventListener('click', (eventoDoClick) => {    
-    eventoDoClick.preventDefault();
-
-    // console.log('----- 1 app.js -----');
+    eventoDoClick.preventDefault();    
 
     const todosInputs = document.querySelectorAll('input'); //pega todos os inputs  
-    //console.log(todosInputs);
-
-    // let i = 1;
+    
     todosInputs.forEach( input => { //intera pelos inputs enviando-os, um a um, a function 'valida';
     
-        // input.addEventListener('click', (eventoDoClick) => {
-            // console.log(`----- forEach ${i}----`);    
-            //console.log(input);        
             valida(input);
-            
-            // i++;
-        // })
+       
     })
-
-    
 
 })   
 
--- js/validacao.js
+++ js/validacao.js
@@ -1,20 +1,62 @@
 
-function valida(input){ // função que recebe os inputs e checa no objeto o tipo de input
-                               //para chamar a function de validação adequada ao input
-
-    // console.log('---- 2 function valida in validacao.js ----')
-    // const tipoDeInput = input.dataset.tipo //usa o 'dataset' para receber o data-attribut do input
-
-    //console.log(input.validity);
-
-    console.log(input.validity.valid);
-
+function valida(input){ // função que recebe os inputs
+ 
+    const dataTipo = input.dataset.tipo //usa o 'dataset' para receber o data-attribut do input    
+    
     if(input.validity.valid){//propriedade valid do input. Se estiver 'false' está vazio.
-        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.classList.remove('input-container--invalido'); //remove a classe css
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''; //add vazio ao conteúdo da tag span
     }else{
-        input.parentElement.classList.add('input-container--invalido');
+        console.log(input.validity);
+        input.parentElement.classList.add('input-container--invalido'); //add a classe css
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(dataTipo, input); //add a mensagem ao conteúdo da tag span
     }
 
+}
+
+//objeto que contém um vetor com os tipos de erro do atributo validity
+const tipoDeErro = [
+    'valueMissing',
+    'typeMismatch'
+    // 'patternMismatch',
+    // 'customError',
+]
+
+//objeto que contém as mensagens para cada tipo de erro de cada data-attribute do form
+const mensagemDeErro ={
+    firstName: {
+        valueMissing: 'First Name cannot be empty'
+    },
+
+    lastName: {
+        valueMissing: 'Last Name cannot be empty'
+    },
+
+    email: {
+        valueMissing: 'Email cannot be empty.',
+        typeMismatch: 'Look like this is not an email'
+    },
+
+    password: {
+        valueMissing: 'Password cannot be empty'        
+    }
+}
+
+function mostraMensagemDeErro(dataTipo, input){
+    let mensagem = '';
+
+    tipoDeErro.forEach(erro => { //chama o objeto 'tipoDeErro' e intera pelos seus elementos
+
+        if(input.validity[erro]){//se ouver algum erro no atributo validity do input, que coincida com um dos elementos 
+                                 // do objeto 'tipoDeErro'...
+                                 
+            mensagem = mensagemDeErro[dataTipo][erro]; //... passa ao objeto 'mensagemDeErro' o dataTipo e o erro, 
+                                                       //para receber a mensagem.
+        }
+        
+    });
+
+    return mensagem;
 }
 
 export default valida
